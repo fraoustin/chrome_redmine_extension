27b641edbc6696a2f7e8c2e11bdcbdd31945c2cf
fraoustin <fraoustin@gmail.com>
2023-12-11 17:49:07 +0100

    <dev> advance ...

--- selectissues.js
+++ selectissues.js
@@ -1,6 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     var newIssue = document.getElementById('newIssue');
-  
+
+    
+    chrome.storage.sync.get(['urlParam'], function(result) {
+        targetURL = result.urlParam || '';
+        console.log(targetURL);
+        targetURL = targetURL + '/issues?per_page=100&sort=due_date%2Cid%3Adesc';
+        console.log(targetURL);
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', targetURL, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    const container = document.createElement('div');
+                    container.innerHTML = xhr.responseText;
+                    const subjectTDs = container.querySelectorAll('td.subject');
+                    subjectTDs.forEach(td => {
+                        console.log(td.textContent);
+                    });
+                } else {
+                    console.error('Erreur HTTP! Statut :', xhr.status);
+                }
+            }
+        };
+        xhr.send();
+    })
+
+
     // Enregistrer la nouvelle valeur du paramètre URL
     newIssue.addEventListener('click', function() {        
         chrome.storage.sync.get(['txtSelect'], function(result) {
